1a10ef98ac31fb493fe47772bc74cd10efaec0a2
ninap41 <ninapalumbo@gmail.com>
2017-09-24 20:05:59 -0500

    For a Few Billion ver 1 added

---
+++ Javascript/Forafewbillion/1st Attempt/script.js
@@ -0,0 +1,33 @@
+//Lotta Penniess
+
+var penny = 1;
+var sum = 0;
+    for (var i =30; i>0;i--){
+        penny = penny * 2;
+        sum = sum + penny;
+    }
+        
+        sum = Math.floor(sum/100);
+        console.log(sum);
+
+// how many days until he makes $10,000?
+
+        
+        
+  
+        
+       
+  
+   
+        
+    
+
+
+// how many days until he makes a BILLION?
+
+
+
+
+
+
+
