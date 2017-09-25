dc8e02a4087291945f8cc23ff8ed97641e90443d
ninap41 <ninapalumbo@gmail.com>
2017-09-24 20:17:00 -0500

    Added If you Don't Mind ver 1

---
+++ Javascript/Time/script.js
@@ -0,0 +1,26 @@
+//what time is it?//
+
+var hour= 7;
+var minute= 50;
+var period = "am";
+
+    console.log("It's" +" ")
+    if (minute > 30){
+        hour=hour+1
+        console.log("almost" + " " + hour)
+    }
+    else (console.log("just" +" " + "after" + " "));
+
+    if (period === "PM"){
+        console.log("in"+ " "+ "the" + " "+"evening");
+    }
+    else (console.log("in"+ " " +"the" + " "+ "morning"));
+
+
+    //Add functionality for "quarter after", "half past", "5 after" ...
+//Distinguish between "in the afternoon", "at night", "noon", "midnight" ...
+    
+    //TBC
+
+
+    
