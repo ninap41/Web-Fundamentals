7239608d3a7024351aa3ad7909c17a8a600ceea2
ninap41 <ninapalumbo@gmail.com>
2017-09-25 13:04:25 -0500

    Added Function NumbersOnly

---
+++ Javascript/NumbersOnly/script.js
@@ -0,0 +1,29 @@
+//numbers only
+    
+    var numbersOnly = function(arr) {
+    var newarr = [];
+    for(var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == "number"){
+            newarr.push(arr[i]);
+          }
+        }
+        return newarr;
+    }
+    var test = numbersOnly([1, "apple",-3,"Orange", 6]);
+    console.log(test);
+
+
+//BONUS positive numbers only
+    var numbersOnly = function(arr) {
+     var newarr = [];
+        for(var i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == "number" && arr[i] > 0){
+              newarr.push(arr[i]);
+              }
+            }
+            return newarr;
+       }
+        var test = numbersOnly([1, "apple",-3,"Orange", 6]);
+        console.log(test);
+
+        
