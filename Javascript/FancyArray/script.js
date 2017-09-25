fa3445c580eabee0c6ed1269c9b42da73829c98d
ninap41 <ninapalumbo@gmail.com>
2017-09-25 15:03:54 -0500

    Added Fancy Array&RangePrint

---
+++ Javascript/FancyArray/script.js
@@ -0,0 +1,10 @@
+
+var fancy = function (arr){
+var newarr=[];
+        for (var i= 0; i< arr.length; i++){
+        newarr.push(arr);    
+            }                
+        return newarr; 
+        }
+var test = fancy(['This','is','a','String']);
+console.log(test);
---
+++ Javascript/RangePrint/script.js
@@ -0,0 +1,13 @@
+function printRange(start, end, iterate){
+    for (var i = start; i < end; i += iterate) {
+        console.log(i)
+    }
+}
+printRange(2, 10, 2);
+
+
+
+
+
+
+
