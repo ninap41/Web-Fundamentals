327dbe459332141bbbce5ff9d2ebb16a27a99054
ninap41 <ninapalumbo@gmail.com>
2017-09-24 20:23:02 -0500

    Added While You Wait ver 1

---
+++ Javascript/Whileyouwait/script.js
@@ -0,0 +1,23 @@
+function Dumb() //(D)ays (U)ntil (M)y (B)irthday :) 
+
+    for (var i = 60; i > 0; i--){
+    while ( i > 30){
+        console.log(i + " " + "Days" + " " + "until" + " " + "my" + " "+ "birthday."+ " " + ":(");
+        i--;
+    }
+    while (i=== 30){
+        console.log("1" + " " + " " + "Month" + " " + "to" + " " + "Go!");
+        i--;
+    }
+        while (i > 5){
+            console.log(i + " " + "Days" + " " + "until" + " " + "my" + " " + "birthday!!");
+        i--;
+    }
+    while (i >=1){
+        console.log(i + " " + "DAYS" + " " + "UNTIL" + " " + "MY" + " " + "BIRTHDAY!!");
+        i--;
+    }
+
+    console.log("PARTY" + " " +  "TIME!!");
+    }
+}
